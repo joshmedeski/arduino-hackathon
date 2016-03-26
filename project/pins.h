46d390dd20a2ae07ad389399d0bdff23eb59d92f
Daniel <djscheufler@gmail.com>
2016-03-26 14:39:59 -0500

    Added default pins.h
    Updated a default pins.h, with pre-assigned pins for use. Currently only Serial, and LED are defined, others are commented out. Please name uniquely and then update this file.

---
+++ project/pins.h
@@ -0,0 +1,25 @@
+#include "arduino.h"
+
+/*** Digital Pins ***/
+#define SERIAL_RX     0
+#define SERIAL_TX     1
+//#define digital2     2 
+//#define digital3     3  //PWM
+//#define digital4     4
+//#define digital5     5  //PWM
+//#define digital6     6  //PWM
+//#define digital7     7 
+//#define digital8     8 
+//#define digital9     9  //PWM
+//#define digital10   10  //PWM
+//#define digital11   11  //PWM
+//#define digital12   12
+#define LED           13
+
+/*** Analog Pins ***/
+//#define analog0  A0
+//#define analog1  A1
+//#define analog2  A2
+//#define analog3  A3
+//#define analog4  A4
+//#define analog5  A5
